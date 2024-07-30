64171c4e5eb5cb1d830d6b50bced70440063893a
Awing <awingding.win@gmail.com>
2024-07-30 14:25:09 +0200

    progress on bot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import src.data.sqlite as data
 from pathlib import Path
+from configparser import ConfigParser
+
+from src import Data, Bot
 
 
 def main() -> None:
-    path = Path().resolve() / "data" / "database_linguistique.db"
-    db = data.Data(str(path))
+    config = parse_config()
+    path = Path().resolve() / config["CONFIG"]["database_rel_path"]
+    db = Data(str(path)).db
+    for entry in db:
+        bot = Bot(entry, config["CONFIG"]["namespace"], db)
+        bot.run()
 
 
+def parse_config() -> ConfigParser:
+    config = ConfigParser()
+    config.read("config.ini")
+    return config
 
 
 if __name__ == '__main__':
--- src/data/sqlite.py
+++ src/data/sqlite.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Data:
     """
     A class to interact with a SQLite database, specifically designed to work with a dictionary database.
@@ -14,7 +15,7 @@ class Data:
         get_next_page(): Returns the next page of results from the database.
     """
 
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, debug=False, limit: int = None, offset: int = None) -> None:
         """
         Initializes the Data class by connecting to the SQLite database and fetching all rows from the 'dictionnaire' table.
 
@@ -24,16 +25,25 @@ class Data:
         self.index = 0
         self.connection = sqlite3.connect(path)
         self.cursor = self.connection.cursor()
-        self.db = self.cursor.execute("""SELECT francais, pierrick, phonétique, classe, commentaire,
+        if debug:
+            self.db = self.cursor.execute(f"""SELECT francais, pierrick, phonétique, classe, commentaire,
                                                 définition, étymologie, cyrilic, hangeul
-                                         FROM dictionnaire""").fetchall()
+                                         FROM dictionnaire
+                                         LIMIT ${limit} OFFSET ${offset}""").fetchall()
+        else:
+            self.db = self.cursor.execute("""SELECT francais, pierrick, phonétique, classe, commentaire,
+                                                définition, étymologie, cyrilic, hangeul
+                                             FROM dictionnaire""").fetchall()
 
-    def get_next_page(self) -> tuple[str]:
+    def get_next_page(self) -> tuple[str] | bool:
         """
         Increments the index and returns the next row from the database.
 
         Returns:
             tuple[str]: A tuple containing the data of the next row in the database.
+            False: If there are no more rows to fetch.
         """
+        if self.index >= len(self.db):
+            return False
         self.index += 1
         return self.db[self.index - 1]
